d7614a65f23e4780881132a99504d70096135271
k2uhl <github@redlagoon.net>
2006-02-27 19:49:00 +0000

    Starting reapply from py5 2.4.  SVN commit message for this first one was:
    Added "pthread.h"--compile was failing on an function we don't use.  So I just copied the standard header & removed the offending line.
    
    This is obviously a kludge.

--- Lib/plat-os400/pthread.h
+++ Lib/plat-os400/pthread.h
@@ -363,8 +363,13 @@ void *pthread_getspecific(pthread_key_t key);
   //int pthread_key_create(pthread_key_t *key,
   //                       void (*destructor)(void *));
 #else                                                           /*@B3A*/
+<<<<<<< HEAD
   //int pthread_key_create(pthread_key_t *__ptr64 key,
   //            void (*__ptr64 destructor)(void *__ptr64));       /*@B3A*/
+=======
+  int pthread_key_create(pthread_key_t *__ptr64 key,
+              void (*__ptr64 destructor)(void *__ptr64));       /*@B3A*/
+>>>>>>> fe59fc7... Added "pthread.h"--compile was failing on an function we don't use.  So I just copied the standard header & removed the offending line.
   #pragma map(pthread_key_create, "pthread_key_create_ts64") /*@B1A@B3M*/
 #endif                                                          /*@B3A*/
 int pthread_setspecific(pthread_key_t key, const void *value);
